8df783168113ef74712a2694af95ab769ce5cd87
John McGonigle <john.mcgonigle@insmed.com>
2023-09-11 17:24:40 +0100

    Updated annotations.

--- crispr_disease_map/construct_annotate_dict.py
+++ crispr_disease_map/construct_annotate_dict.py
@@ -1,4 +1,5 @@
 import json
+from collections import defaultdict
 import os.path
 
 import pandas as pd
@@ -15,28 +16,32 @@ if __name__ == "__main__":
     print(annotation_data_folder + '*.tsv')
 
     replicate_to_sample = {}
-    replicate_to_filename = {}
+    replicate_to_filename = defaultdict(list)
     sample_to_annotation = {}
     for file in glob(annotation_data_folder + '*.xlsx'):
         print(file)
         df = pd.read_excel(file, 'Sample details')
-        
+        filename = os.path.basename(file).replace('.xlsx', '.counts.tsv')
         for replicate, sample, treatment, dose, days_grown, cell_line, ko in \
             zip(df['Replicate'], df['Sample'], df['Treatment'], df['Dose'], df['Days grown'], df['Cell line'], df['KO']):
             
-            replicate_to_sample[replicate] = sample
+            replicate_to_sample[f"{replicate}_{filename}"] = sample
 
-            filename = os.path.basename(file).replace('.xlsx', '.counts.tsv')
+
             assert(os.path.isfile(counts_data_folder + filename))
-            replicate_to_filename[replicate] = filename
-            
-            sample_to_annotation[sample] = {'Replicate': replicate,
-                                            'Sample': sample,
-                                            'Treatment': treatment,
-                                            'Dose': dose,
-                                            'Days grown': days_grown,
-                                            'Cell line': cell_line,
-                                            'KO': ko}
+            replicate_to_filename[filename].append(replicate)
+
+
+
+            if sample not in sample_to_annotation:
+                sample_to_annotation[sample] = {'Replicate': list(),
+                                                'Sample': sample,
+                                                'Treatment': treatment,
+                                                'Dose': dose,
+                                                'Days grown': days_grown,
+                                                'Cell line': cell_line,
+                                                'KO': ko}
+            sample_to_annotation[sample]["Replicate"].append(replicate)
 
     # pprint(replicate_to_sample)
     # pprint(sample_to_annotation)
